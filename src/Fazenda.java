615fe6ea631b82e566ed81554c86215737282222
AndreyLima-066 <andreyalmeidalima066@gmail.com>
2025-09-30 14:11:39 -0300

    Update Fazenda.java

--- src/Fazenda.java
+++ src/Fazenda.java
@@ -1,5 +1,27 @@
-public class Main {
+import java.util.Scanner;
+public class Fazenda {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("--- Bem-Vindo ao Fazenda Ednaldo Paes ---");
+        System.out.println("Escolha um animal para visitar:\n1 - Cachorro \n2 - Vaca \n3 - Gato \n4 - Sair");
+        int escolha = scanner.nextInt();
+        Animal AnimalSelecionado = null;
+
+
+        if (escolha == 1) {
+            AnimalSelecionado = new Cachorro();
+        } else if (escolha == 2) {
+            AnimalSelecionado = new Vaca();
+        } else if (escolha == 3) {
+            AnimalSelecionado = new Gato();
+        } else if (escolha == 4) {
+            System.out.println("saindo");
+            scanner.close();
+            return;
+
+        }
+
+        AnimalSelecionado.visitar();
     }
+
 }
