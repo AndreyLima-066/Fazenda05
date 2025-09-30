3cc78fca4c8534b1224dbe203d1e77262eaf312d
AndreyLima-066 <andreyalmeidalima066@gmail.com>
2025-09-30 14:12:15 -0300

    Update Cachorro.java

--- src/Cachorro.java
+++ src/Cachorro.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
-public class Galinha {
+public class Cachorro extends Animal {
+    @Override
+    public void visitar() {
+        System.out.println("AU! AU! AU!");
+    }
 }
